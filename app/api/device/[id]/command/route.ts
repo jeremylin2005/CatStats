f6703c30d9c24b5c0cc9651a181a626b1fe709d2
jeremy, lin <jeremylin.work@gmail.com>
2026-04-30 17:01:59 -0700

    test

--- app/api/device/[id]/command/route.ts
+++ app/api/device/[id]/command/route.ts
@@ -4,20 +4,20 @@ import { getFood } from "@/lib/feeding"
 import { getWater } from "@/lib/refill"
 
 export async function GET(req: NextRequest, context: { params: { id: string }}){
-    const deviceID = context.params.id.toUpperCase()
-    return NextResponse.json({ hit: "deviceID" })
-    let command: DeviceCommand
+    // const deviceID = context.params.id.toUpperCase()
+    // let command: DeviceCommand
 
-    if(deviceID == "FOOD"){
-        command = await getFood(deviceID)
-    } else if(deviceID == "WATER"){
-        command = await getWater(deviceID)
-    } else {
-        command = {
-            command: "ERROR",
-            reason: "UNKNOWN_DEVICE_ID"
-        }
-    }
+    // if(deviceID == "FOOD"){
+    //     command = await getFood(deviceID)
+    // } else if(deviceID == "WATER"){
+    //     command = await getWater(deviceID)
+    // } else {
+    //     command = {
+    //         command: "ERROR",
+    //         reason: "UNKNOWN_DEVICE_ID"
+    //     }
+    // }
 
-    return NextResponse.json(command)
+    // return NextResponse.json(command)
+    return Response.json({ id: context.params.id })
 }   
