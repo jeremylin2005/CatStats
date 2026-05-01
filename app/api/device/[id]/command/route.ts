cc6bf40d4eea394db8d1078c2372ae9caec359b8
jeremy, lin <jeremylin.work@gmail.com>
2026-04-30 17:27:47 -0700

    revise

--- app/api/device/[id]/command/route.ts
+++ app/api/device/[id]/command/route.ts
@@ -3,29 +3,21 @@ import { DeviceCommand } from "@/lib/types"
 import { getFood } from "@/lib/feeding"
 import { getWater } from "@/lib/refill"
 
-// export async function GET(req: NextRequest, { params }: { params: { id: string }}){
-//     const deviceID = params.id
-//     // let command: DeviceCommand
+export async function GET(req: NextRequest, context: { params: Promise<{ id: string }>}){
+    const { id } = await context.params
+    const deviceID = id
+    let command: DeviceCommand
 
-//     // if(deviceID == "FOOD"){
-//     //     command = await getFood(deviceID)
-//     // } else if(deviceID == "WATER"){
-//     //     command = await getWater(deviceID)
-//     // } else {
-//     //     command = {
-//     //         command: "ERROR",
-//     //         reason: "UNKNOWN_DEVICE_ID"
-//     //     }
-//     // }
+    if(deviceID == "FOOD"){
+        command = await getFood(deviceID)
+    } else if(deviceID == "WATER"){
+        command = await getWater(deviceID)
+    } else {
+        command = {
+            command: "ERROR",
+            reason: "UNKNOWN_DEVICE_ID"
+        }
+    }
 
-//     // return NextResponse.json(command)
-//     return Response.json({ id: deviceID })
-// }   
-
-export async function GET(
-  req: Request,
-  context: { params: Promise<{ id: string }> }
-) {
-  const { id } = await context.params
-  return Response.json({ id })
-}
+    return NextResponse.json(command)
+}   
